bdffed52123919cea1b3685d57fc6b89e51264e2
kamm7476-ops <kamm7476@gmail.com>
2026-03-24 17:30:20 +0900

    Update user.js

--- user.js
+++ user.js
@@ -3,6 +3,10 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  
+  // 🌟 [추가됨!] 아티스트명(별명) 저장하는 칸!
+  nickname: { type: String, default: "" }, 
+  
   // 🌟 핵심: 가입하면 무조건 'user'(일반 유저)로 설정됨
   role: { type: String, default: 'user' }, 
   createdAt: { type: Date, default: Date.now } 
