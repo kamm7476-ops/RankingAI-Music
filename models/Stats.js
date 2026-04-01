a51c3043380ffa184cb3ebc99e6d46378ac4e424
kamm7476-ops <kamm7476@gmail.com>
2026-04-01 16:08:40 +0900

    Update Stats.js

--- models/Stats.js
+++ models/Stats.js
@@ -1,1 +1,11 @@
+const mongoose = require('mongoose');
 
+const statsSchema = new mongoose.Schema({
+    date: { type: String, required: true, unique: true }, // 예: '2026-04-01'
+    dailyVisitors: { type: Number, default: 0 },
+    totalVisitors: { type: Number, default: 0 },
+    dailyPlays: { type: Number, default: 0 },
+    totalPlays: { type: Number, default: 0 }
+});
+
+module.exports = mongoose.model('Stats', statsSchema);
